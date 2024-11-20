30349d83f4ae3624a45ce72bab2143efd729d0a8
Adrita Samanta <adrita@Adritas-MacBook-Pro-2.local>
2024-11-20 16:37:17 -0500

    revert to original

--- socialNetwork/src/ComposePostService/ComposePostService.cpp
+++ socialNetwork/src/ComposePostService/ComposePostService.cpp
@@ -108,7 +108,5 @@ int main(int argc, char *argv[]) {
       std::make_shared<TFramedTransportFactory>(),
       std::make_shared<TBinaryProtocolFactory>());
   LOG(info) << "Starting the compose-post-service server ...";
-  LOG(info) << "Initial update message ...";
   server.serve();
-  LOG(info) << "Updated ComposePostService Test ...";
 }
